cb0595117c828893aabc59c8da7e3490e60854ec
riya-arora08 <72336481+riya-arora08@users.noreply.github.com>
2020-10-04 16:26:52 +0530

    updated palindrome

--- Basic Programs/Palindrome number.cpp
+++ Basic Programs/Palindrome number.cpp
@@ -1,18 +1,17 @@
 //program to check if a number is palindrome
-
+//A palindromic number is a number that remains the same when its digits are reversed.
 #include <iostream>  
 using namespace std;  
 int main()  
 {  
   int n,r,sum=0,temp;    
-  cout<<"Enter the Number=";    
+  cout<<"Enter the Number = ";    
   cin>>n;    
  temp=n;    
- while(n>0)    
+ for( ; n>0 ; n=n/10)    
 {    
  r=n%10;    
- sum=(sum*10)+r;    
- n=n/10;    
+ sum=(sum*10)+r;
 }    
 if(temp==sum)    
 cout<<"Number is Palindrome.";    
